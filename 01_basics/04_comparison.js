e9313fb38ca362a146cffc326fabb00556e7a6dd
webDevDhruv <157681359+webDevDhruv@users.noreply.github.com>
2024-02-12 17:24:11 +0000

    learned about value comparison with comparison operators and do's and dont's with different datatype comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// comparison operators
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+//make sure that data type of value that you compare are same or else it's bad coading practice
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === checks both the value and it's data type as well
+console.log("2" === 2);
