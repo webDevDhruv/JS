0d37c6397215c1eb5a821bbc3b91bef9f8977042
webDevDhruv <157681359+webDevDhruv@users.noreply.github.com>
2024-02-12 09:42:29 +0000

    learned about dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3); // we are using  nodeJS, not browser.
+
+console.log( 3 
+    + 
+    3); // code readability shoudde be prioritized
+
+console.log("Dhruv");
+
+// data types in js
+/*
+number
+bigint
+string => ""
+boolean => true/false
+null => standalone value
+undefined
+symbol => unique
+
+object
+*/
+
+console.log(typeof null); // output => object
+console.log(typeof undefined); // output => undefined
+
+let name = "Dhruv"; // string data type
+let age = 25; //number data type
+let isLoggedIn = true //boolean data type
+
