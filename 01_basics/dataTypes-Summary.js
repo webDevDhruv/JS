c5d8484881647f299252974a513be247a921fb07
webDevDhruv <157681359+webDevDhruv@users.noreply.github.com>
2024-02-12 18:06:23 +0000

    dataTypes summary

---
+++ 01_basics/dataTypes-Summary.js
@@ -0,0 +1,49 @@
+// Primitive data types
+
+/*
+String
+Number
+Boolean
+null
+undefined
+Symbol
+BigInt
+*/
+
+//dataType examples
+const name = "Dhruv" //String
+const score = 100; //Number
+const scoreValue = 100.3 //Number
+const isLoggedIn = true //boolean value only can be true or false
+const outsideTemp = null //null = completely empty
+let userEmail; //undefined = not defined yet
+
+// even when we use same value in two different id it's still different from eachother in symbol dataType
+const id = Symbol('123'); //symbol
+const anotherId = Symbol('123'); //symbol
+
+// console.log(id === anotherId); //answer is false
+
+const bigNumber = 3578951234567895123456n; //BigInt
+
+
+// Reference (Non Primitive) types
+/*
+Array
+Objects
+Functions
+*/
+
+const carBrands = ["BMW", "Lamborghini", "Koenigsegg", "Pagani", "Ferrari"] //Array
+let myInfo = {
+    name: "Dhruv",
+    age: 25
+};
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+// how to check dataType of a value
+console.log(typeof id);
+console.log(typeof(carBrands));
