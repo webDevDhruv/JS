ab647974fea7250c1abc324ba191dbd7ac661bca
webDevDhruv <157681359+webDevDhruv@users.noreply.github.com>
2024-02-15 06:56:08 +0000

    learned about functions, it's parameters and functions with objects and arrays

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -36,4 +36,50 @@ function addTwoNumbers(number1, number2) {
 // addTwoNumbers(3, 4);
 
 const result = addTwoNumbers(5, 3);
-console.log("result:", result);
+// console.log("result:", result);
+
+function loginUserMessage(username){
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("Dhruv"));
+
+// if we wont give arguments to a function it will give the undefined as a result
+// console.log(loginUserMessage());
+
+//if we want multiple values with one function perameter we can use rest operator or spread operator(...peramaeter)
+function calculateCartPrice(...num1){
+    return num1;
+};
+
+//if we use multiple parameters and add spread or rest operator after first two values it won't take the values before rest or spread operator
+//it will ignore the val1 and val2 only returns last two value ([300, 2000]). because 200 and 400 goes to val1 and val2 and rest in ...num1 so only ...num1 values will be returned
+// function calculateCartPrice(val1, val2, ...num1){
+//     return val1, val2, num1;
+// };
+
+//we only get the first value because there is only one perameter in the function defination
+// console.log(calculateCartPrice(200, 400, 300, 2000));
+
+const user = {
+    userName: "Dhruv",
+    coursePrice: 5537
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.userName} and course price is ${anyObject.coursePrice}`);
+}
+
+// handleObject(user);
+
+//  handleObject({userName: "Khyati", coursePrice: 5537})
+
+const myNewArray = [20, 55, 70, 90];
+
+function returnSeconValue (getArray){
+    return getArray[1];
+};
+
+// console.log(returnSeconValue(myNewArray));
+
+console.log(returnSeconValue([20, 55, 70, 90]));
