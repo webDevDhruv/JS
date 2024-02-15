2fd9e2c439eaaddcd3406cbc6140ce92df2ab151
webDevDhruv <157681359+webDevDhruv@users.noreply.github.com>
2024-02-15 09:28:45 +0000

    learned about scope and basic hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,14 +1,63 @@
+//this is global scope variables and we can access it in functions as well
 let a = 10;
 const b = 20;
 // var c = 30;
 
+//block scope variables only accessible inside a block of code
+//the variables we defined inside the if statement are only accessible inside the curly breaces and we call it block level scope 
+//but with var keyword we can access variable anywhere in the code and it's a bad practice.
+//Note:- Don't use var keyword
 if(true){
-    let a = 10;
+    // let a = 10;
     const b = 20;
+    //you can access a because it's in the block of code
+    //if a was not defined in block still we have (a) at global scope so we can access it in side block scope
+    // console.log("inner", a);
     var c = 30;
 }
 
-
+//you can see we can access variable (c) even when it's inside a block 
 // console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
+
+function one(){
+    const userName = "Dhruv"
+
+    function two(){
+        const website = "YouTube"
+        console.log(userName);
+    }
+    // console.log(website);
+
+    two()
+}
+
+// one()
+
+if (true) {
+    const userName = "Dhruv";
+    if (userName === "Dhruv") {
+        const website = " YouTube";
+        // console.log(userName + website);
+    }
+    // console.log(website);
+}
+
+// console.log(userName);
+
+// ++++++++++++++++++++++ interesting ++++++++++++++++++++++
+
+// basic example of hoisting
+console.log(addOne(5));
+function addOne (num) {
+    return num + 1;
+}
+
+// when the function is inside of a variable we cannot use Hoisting
+// addTwo(5)
+const addTwo = function (num) {
+    return num + 2;
+}
+
+console.log(addTwo(5));
