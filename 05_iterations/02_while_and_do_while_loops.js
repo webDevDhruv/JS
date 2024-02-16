1f7a97294f9c3f74a651605434a93704500d79eb
webDevDhruv <157681359+webDevDhruv@users.noreply.github.com>
2024-02-16 18:58:37 +0000

    learned about while and do while loops

---
+++ 05_iterations/02_while_and_do_while_loops.js
@@ -0,0 +1,22 @@
+// while loops
+
+let index = 0;
+while (index <= 10) {
+    // console.log(`value of index is ${index}`);
+    index = index + 2;
+}
+
+let cars = ["BMW", "Lamborghini", "Koenigsegg", "Pagani", "Ferrari"];
+let arr = 0;
+while (arr < cars.length) {
+    // console.log(`Value is ${cars[arr]}`);
+    arr = arr + 1;
+}
+
+// do while loop
+
+let score = 11;
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
