db430deda832220e47e635be75c376a3d2627c3e
webDevDhruv <157681359+webDevDhruv@users.noreply.github.com>
2024-03-26 06:23:59 +0000

    learned about promises

--- 08_advance/promises.js
+++ 08_advance/promises.js
@@ -42,9 +42,64 @@ const promiseFour = new Promise( (resolve, reject) => {
     },1000);
 });
 
-promiseFour.then( (user) => {
+promiseFour
+.then( (user) => {
     console.log(user);
     return user.username;
-}).then( (username) => {
+})
+.then( (username) => {
     console.log(username);
 })
+.catch ( (error) => {
+    console.log(error);
+})
+.finally( () => {
+    console.log('The promise is either resolved or rejected');
+});
+
+
+const promiseFive = new Promise (function(resolve, reject){
+    setTimeout( () => {
+        let error = true;
+        if(!error){
+            resolve({username: 'javascript', password: '123'})
+        }else {
+            reject('ERROR: JS went wrong');
+        }
+    },1000);
+});
+
+async function consumePromiseFive() {
+    try {
+        const response = await promiseFive;
+    console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+consumePromiseFive();
+
+// async function getAllUsers() {
+    // try {
+    //     const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    //     const data = await response.json()
+    //     console.log(data);
+    // } catch (error) {
+    //     console.log("E:", error);
+    // }
+    
+// };
+
+// getAllUsers();
+
+fetch("https://api.github.com/users/hiteshchoudhary")
+.then( (response) => {
+    return response.json()
+})
+.then( (data) => {
+    console.log(data);
+})
+.catch( (error) => {
+    console.log(error);
+});
